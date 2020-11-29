d69db44c4b7e1b4bcb25e57c8e0646d305056b39
bjsmak <brian.j.smak@gmail.com>
2020-11-29 15:55:53 -0500

    add schema

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX depart_index (department_id),
+    CONSTRAINT FK_depart FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    INDEX role_index (role_id),
+    CONSTRAINT FK_role FOREIGN KEY (role_id) REFERENCES role(id)
+);
