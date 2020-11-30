03d025faa1de5aaf179d6b7b3a84cd8b403fcb16
bjsmak <brian.j.smak@gmail.com>
2020-11-30 18:25:22 -0500

    added user selection, added functions for list views

--- employee-tracker.js
+++ employee-tracker.js
@@ -19,5 +19,53 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
 if (err) throw err;
+console.log("Connected");
 employeeSearch();
 });
+
+function employeeSearch() {
+    inquirer.prompt({
+        message: "Please select what you would like to perform",
+        type: "list",
+        name: "selection",
+        choices: [
+            "View Employee List",
+            "View Department List",
+            "View Role List",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Employee Role",
+            "End"
+        ]
+    }).then(answer => {
+        switch (answer.choice) {
+            case "View Employee List":
+                employeeList()
+                break;
+            
+            case "View Department List":
+                departmentList()
+                break;
+
+            case "View Role List":
+                RoleList()
+                break;
+        }
+    })
+}
+
+function employeeList() {
+    connection.query("SELECT * FROM employee", function (err, data) {
+        console.table(data);
+        employeeSearch();
+    })
+}
+
+function departmentList() {
+    connection.query("SELECT * FROM department", function (err, data) {
+        if (err) throw err;
+        console.table(data);
+        employeeSearch();
+    })
+}
